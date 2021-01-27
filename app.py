1746801e99b409501133b80bcaaa4414c2bd1e7c
demidraksin <greatraksin@icloud.com>
2021-01-27 11:12:17 +0300

    Adding DB configuration and dependencies🦷

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
