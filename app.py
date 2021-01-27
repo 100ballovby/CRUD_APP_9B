6c3a3dedb4ddb5ac4e0b42ff36364913d49e11ab
demidraksin <greatraksin@icloud.com>
2021-01-27 11:22:49 +0300

    Adding grocery class. Define table in DB🐍

--- app.py
+++ app.py
@@ -8,6 +8,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
+class Grocery(db.Model):
+    """Класс описывает таблицу в БД"""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        """Когда происходит запрос, БД возвращает имя объекта из таблицы"""
+        return f'<Grocery {self.name}>'
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
